52a04fa8e8144d94acf52735b7240ce51263fada
Tam Nguyen M <nmint8m@gmail.com>
2019-07-06 17:05:22 +0700

    Internationalization and Localization - Change language by using AppleLanguages in UserDefaults

---
+++ DaijobuExample/Daijobu/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Daijobu
+//
+//  Created by Tam Nguyen M. on 7/6/19.
+//  Copyright © 2019 Tam Nguyen M. All rights reserved.
+//
+
+import UIKit
+
+UIApplicationMain(CommandLine.argc,
+                  CommandLine.unsafeArgv,
+                  NSStringFromClass(UIApplication.self),
+                  NSStringFromClass(AppDelegate.self))
